a402b4223b26dd566a127a4ba2c3ec5fe0930bb0
aispark <aispark@paran.com>
2020-07-06 14:53:02 +0900

    토큰 정보 추가

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,15 +1,44 @@
-import axios from 'axios';
+import axios from "axios";
 
 // Add custom configuration here.
 const axiosInstance = axios.create({
-
-    baseURL: process.env.VUE_APP_CONTEXT,
-    timeout: 30000,
-    headers: {
-        'Content-Type': 'application/json',
-        'x-auth-token': localStorage.getItem('x-auth-token')
-    },
-
+  baseURL: process.env.VUE_APP_CONTEXT,
+  timeout: 30000,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
+
+axiosInstance.interceptors.request.use(
+  async function(config) {
+    //토큰이 있을 경우 헤더에 포함한다.
+    config.headers["x-auth-token"] = localStorage.getItem("x-auth-token");
+    return config;
+  },
+  function(error) {
+    return Promise.reject(error);
+  }
+);
+
+//토큰 만료 처리
+axiosInstance.interceptors.response.use(
+  function(response) {
+    return response;
+  },
+  async function(error) {
+    const { config } = error;
+    console.log("axios respone error", config);
+    if (error.response.data.status === 401 && config.retry === undefined) {
+      config.retry = true;
+
+      //토큰 갱신
+      //await refreshToken();
+
+      //실패한 request 재실행
+      //   return await axiosInstance(config);
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
