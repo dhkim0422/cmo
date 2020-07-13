1e7ef7d0b258dfdb8eb0c8ef5a1496d370c1495a
aispark <aispark@paran.com>
2020-07-13 10:22:49 +0900

    라우팅 추가

--- src/router.js
+++ src/router.js
@@ -1,100 +1,102 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/views/Home.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 //lazy Loading
 const Login = () => {
-    return import('@/views/Login.vue')
-}
+  return import("@/views/Login.vue");
+};
 const ResistanceGenes = () => {
-    return import('@/views/ResistanceGenes.vue')
-}
+  return import("@/views/ResistanceGenes.vue");
+};
 const ResistanceGenesDetail = () => {
-    return import('@/views/ResistanceGenesDetail.vue')
-}
+  return import("@/views/ResistanceGenesDetail.vue");
+};
 const ResistanceGenesRegister = () => {
-    return import('@/views/ResistanceGenesRegister.vue')
-}
+  return import("@/views/ResistanceGenesRegister.vue");
+};
 const VueTree = () => {
-    return import('@/views/VueTree.vue')
-}
-
+  return import("@/views/VueTree.vue");
+};
 
 const requireAuth = (returnPath) => (from, to, next) => {
-    const isAuthenticated = false
+  const isAuthenticated = false;
 
-    if (isAuthenticated) return next()
-    next('/login')
-}
-
+  if (isAuthenticated) return next();
+  next("/login");
+};
 
 export default new Router({
-    mode: 'history',
-    base: '/cmo',
-    routes: [
-        {
-            path: '/',
-            name: 'home',
-            component: Home
-        },
-        {
-            path: '/index.html',
-            name: 'indexHome',
-            component: Home
-        },
+  mode: "history",
+  base: "/cmo",
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: Home,
+    },
+    {
+      path: "/index.html",
+      name: "indexHome",
+      component: Home,
+    },
 
-        {
-            path: '/login',
-            name: 'login',
-            component: Login
-        },
-        {
-            path: '/analysis/analysis',
-            name: 'analysis',
-            component: () => import('./views/Analysis.vue')
-
-        },
-        {
-            path: '/targets/targetList',
-            name: 'targets list',
-            component: () => import('./views/targets/TargetsList.vue')
-
-        },
-        {
-            path: '/targets/targets-detail/:id',
-            name: 'targets-detail',
-            component: () => import('./views/targets/TargetDetail.vue')
-        },
-        {
-            path: '/project/projectList',
-            name: 'project list',
-            component: () => import('./views/project/ProjectList.vue')
-
-        },
-        {
-            path: '/project/projectsDetail/:id',
-            name: 'projects-detail',
-            component: () => import('./views/project/projectsDetail')
-
-        },
-        {
-            path: '/project/ProjectRegist',
-            name: 'projects regist',
-            component: () => import('./views/project/ProjectRegist')
-
-        },
-        {
-            path: '/samples/SamplesList',
-            name: 'Samples List',
-            component: () => import('./views/samples/SamplesList')
-
-        },
-        {
-            path: '/samples/SamplesDetail/:id',
-            name: 'Samples-detail',
-            component: () => import('./views/samples/SamplesDetail')
-        },
-    ]
-})
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/analysis/analysis",
+      name: "analysis",
+      component: () => import("./views/Analysis.vue"),
+    },
+    {
+      path: "/targets/targetList",
+      name: "targets list",
+      component: () => import("./views/targets/TargetsList.vue"),
+    },
+    {
+      path: "/targets/targets-detail/:id",
+      name: "targets-detail",
+      component: () => import("./views/targets/TargetDetail.vue"),
+    },
+    {
+      path: "/project/projectList",
+      name: "project list",
+      component: () => import("./views/project/ProjectList.vue"),
+    },
+    {
+      path: "/project/projectsDetail/:id",
+      name: "projects-detail",
+      component: () => import("./views/project/projectsDetail"),
+    },
+    {
+      path: "/project/ProjectRegist",
+      name: "projects regist",
+      component: () => import("./views/project/ProjectRegist"),
+    },
+    {
+      path: "/samples/SamplesList",
+      name: "Samples List",
+      component: () => import("./views/samples/SamplesList"),
+    },
+    {
+      path: "/samples/SamplesDetail/:id",
+      name: "Samples-detail",
+      component: () => import("./views/samples/SamplesDetail"),
+    },
+    {
+      path: "/storage/StorageList",
+      name: "StorageList",
+      component: () => import("./views/storage/StorageList"),
+    },
+    {
+      path: "/storage/StorageDetail/:id",
+      name: "StorageDetail",
+      component: () => import("./views/storage/StorageDetail"),
+    },
+  ],
+});
